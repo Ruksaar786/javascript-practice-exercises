603d891289615bca9a3a8ff024a97b262f35118c
adamruksaar1@gmail.com <adamruksaar1@gmail.com>
2024-01-23 09:51:55 +0200

    added files

---
+++ variables_exercise/four.js
@@ -0,0 +1,5 @@
+let movie = "The Rookie";
+
+let views = 4;
+
+console.log("I watched " + movie + " about " + views + " number of times");
---
+++ variables_exercise/zero.js
@@ -0,0 +1,5 @@
+let location = "Brooklyn, " + "NY";
+console.log("Brooklyn NY");
+
+let quantity = 4 * 5 + 1;
+console.log(21);
