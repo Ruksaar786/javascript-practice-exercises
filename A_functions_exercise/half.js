206c4fb99a4b35f254c726c075fdc6a7b322ec40
adamruksaar1@gmail.com <adamruksaar1@gmail.com>
2024-01-25 13:40:09 +0200

    added practice files

---
+++ A_functions_exercise/half.js
@@ -0,0 +1,10 @@
+//write a function 'Half' that accepts a number as an argument. the Function should return half of the
+//number.
+
+const half = function (num) {
+  return num / 2;
+};
+
+console.log(half(8)); //4
+console.log(half(15)); //7.5
+console.log(half(90)); //45
---
+++ A_functions_exercise/snippet-0-1.js
@@ -0,0 +1,26 @@
+//snippet 0-1
+let greet = function () {
+  //greet is a variable
+  console.log("hey");
+  console.log("programmers");
+};
+
+let whistle = function () {
+  console.log("'doot");
+};
+
+console.log("first");
+console.log("second");
+greet();
+console.log("third");
+console.log("fourth");
+whistle();
+
+// print order:
+//first
+//second
+//hey
+//programmers
+//third
+//fourth
+//doot
