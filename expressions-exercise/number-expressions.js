3257565b7ed585c1f66a81d35a07acf4468927ee
adamruksaar1@gmail.com <adamruksaar1@gmail.com>
2024-01-29 11:05:57 +0200

    updated files

--- expressions-exercise/number-expressions.js
+++ expressions-exercise/number-expressions.js
@@ -8,10 +8,10 @@ console.log(4 + 2 * 3); //10
 console.log((4 + 2) * 3); //18
 console.log(5 % 2); //1
 
-console.log(6 % 2); //1
-console.log(7 % 2); //0
-console.log(8 % 2); //1
-console.log(19 % 8); //0
+console.log(6 % 2); //0
+console.log(7 % 2); //1
+console.log(8 % 2); //0
+console.log(19 % 8); //3
 
 console.log(24 % 8); //0
 console.log(7 % 4); //3
