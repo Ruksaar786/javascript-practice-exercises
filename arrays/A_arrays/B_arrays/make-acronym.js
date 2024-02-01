29c3617e217d7e3f3a82f467752cd7815d66f60a
adamruksaar1@gmail.com <adamruksaar1@gmail.com>
2024-02-01 19:02:28 +0200

    added files

---
+++ arrays/A_arrays/B_arrays/make-acronym.js
@@ -0,0 +1,17 @@
+// Write a function `makeAcronym` that accepts a sentence string as an argument. The function should
+// return a string containing the first character of each word in the sentence.
+
+const makeAcronym = function (str) {
+  let acc = "";
+  let arr = str.split(" ");
+  for (let i = 0; i < arr.length; i++) {
+    let firstChar = arr[i][0];
+    acc += firstChar;
+  }
+  return acc.toUpperCase();
+};
+
+console.log(makeAcronym("New York")); // NY
+console.log(makeAcronym("same stuff different day")); // SSDD
+console.log(makeAcronym("Laugh out loud")); // LOL
+console.log(makeAcronym("don't over think stuff")); // DOTS
