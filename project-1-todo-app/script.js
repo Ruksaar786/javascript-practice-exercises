77973862f5757fcb9dfe8aff366a871a924a5bcf
adamruksaar1@gmail.com <adamruksaar1@gmail.com>
2024-02-29 09:53:44 +0200

    added project files

--- project-1-todo-app/script.js
+++ project-1-todo-app/script.js
@@ -5,6 +5,9 @@ let todos = [
   { id: 1, title: "lets go walk", dueDate: "2024-02-28" },
   { id: 2, title: "go for shopping", dueDate: "2024-02-29" },
 ];
+let getData = JSON.parse(localStorage.getItem("todos"));
+
+let todo = getData;
 
 //add todo
 
@@ -12,11 +15,17 @@ let btn = document.getElementById("btn");
 
 btn.addEventListener("click", addTodo);
 
+function storeData() {
+  localStorage.setItem("todo", JSON.stringify(todos));
+}
+
 function addTodo() {
   let title = document.getElementById("title").value;
   let dueDate = document.getElementById("dueDate").value;
   let id = new Date().getTime();
   todos.push({ id: id, title: title, dueDate: dueDate });
+  document.getElementById("title").value = "";
+  storeData();
   render();
 }
 
@@ -25,11 +34,42 @@ function addTodo() {
 function updateTodo(e) {
   e.preventDefault();
   let id = Number(e.target.id);
-  let data = todos.filter((todo) => todo.id === id);
-  console.log(data);
+  let data = todos.filter((todo, index) => {
+    indexValue = index;
+    return todo.id === id;
+  });
+  document.getElementById("title").value = data[0].title;
+  document.getElementById("btn");
+  document.getElementById("btn").style = "display:none";
+  let ubtn = document.createElement("button");
+  ubtn.textContent = "saveUpdate";
+  ubtn.id = "updateId";
+  ubtn.onclick = saveTodo;
+  let form = document.getElementById("form");
+  form.append(ubtn);
+}
+
+//save todo
+
+function saveToDo() {
+  document.getElementById("updateId").style = "display:none";
+  document.getElementById("btn").style = "display:block";
+  let title = document.getElementById("title").value;
+  let obj = todos[indexValue];
+
+  todos[indexValue] = { ...obj, title: title };
+  storeData();
+  render();
 }
 
 //delete
+
+function deleteTodo(e) {
+  let id = Number(e.target.id);
+  todos = todos.filter((todo) => todo.id !== id);
+  storeData();
+  render();
+}
 
 // view/ read/ display
 
@@ -45,6 +85,12 @@ const render = function () {
     updateBtn.style = "margin-left:12px; margin-top:12px";
     updateBtn.onclick = updateTodo;
     div.append(updateBtn);
+    let deletebtn = document.createElement("button");
+    deletebtn.textContent = "Delete todo";
+    deletebtn.id = todo.id;
+    deletebtn.onclick = deleteTodo;
+
+    div.append(deletebtn);
     render.append(div);
   });
 };
