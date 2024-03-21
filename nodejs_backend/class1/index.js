5bd0b31aee5c98fd6088452372b0ed1ee54162af
adamruksaar1@gmail.com <adamruksaar1@gmail.com>
2024-03-21 19:04:53 +0200

    https

---
+++ nodejs_backend/class1/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+function calculateSum(n) {
+  let ans = 0;
+  for (let i = 1; i <= n; i++) {
+    ans = ans + i;
+  }
+  return ans;
+}
+
+app.get("/", (req, res) => {
+  const n = req.query.n;
+  const ans = calculateSum(n);
+
+  res.send(ans);
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
