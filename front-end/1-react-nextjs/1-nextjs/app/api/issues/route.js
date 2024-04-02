069eba1ac9a54b9cb7ef344888903a7cc7c76a52
adamruksaar1@gmail.com <adamruksaar1@gmail.com>
2024-04-02 23:03:21 +0200

    nextjs

---
+++ front-end/1-react-nextjs/1-nextjs/app/api/issues/route.js
@@ -0,0 +1,4 @@
+import { NextRequest, NextResponse } from "next/server";
+export const GET = async (NextRequest) => {
+  return NextResponse.json({ message: "This is get request" });
+};
---
+++ front-end/1-react-nextjs/1-nextjs/components/button.js
@@ -0,0 +1,5 @@
+function Button() {
+  return <button>Click here </button>;
+}
+
+export default Button();
